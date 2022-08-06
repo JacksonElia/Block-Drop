c78c9d0c8576ad18cd32d154398e131e52af3ce5
Traptricker <jackgeorgeelia@gmail.com>
2022-08-06 15:55:40 -0400

    Setup

---
+++ Block Drop/Views/BlockGridView.swift
@@ -0,0 +1,20 @@
+//
+//  BlockGridView.swift
+//  Block Drop
+//
+//  Created by Jack Elia on 8/6/22.
+//
+
+import SwiftUI
+
+struct BlockGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlockGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockGridView()
+    }
+}
---
+++ Block Drop/Views/BlockHoldView.swift
@@ -0,0 +1,20 @@
+//
+//  BlockHoldView.swift
+//  Block Drop
+//
+//  Created by Jack Elia on 8/6/22.
+//
+
+import SwiftUI
+
+struct BlockHoldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlockHoldView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockHoldView()
+    }
+}
