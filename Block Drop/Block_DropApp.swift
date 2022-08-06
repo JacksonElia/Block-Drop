220034144e6a2b3952c185dc0c60ae92126ecf8b
Traptricker <jackgeorgeelia@gmail.com>
2022-08-05 22:47:48 -0400

    Initial Commit

---
+++ Block Drop/Block_DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_DropApp.swift
+//  Block Drop
+//
+//  Created by Jack Elia on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Block_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
