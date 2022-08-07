ac7d2830a601f8a39ac97693894cc2adbf9f16c6
Traptricker <jackgeorgeelia@gmail.com>
2022-08-07 00:13:26 -0400

    Some more changes before I test something

--- Block Drop/Views/BlockHoldView.swift
+++ Block Drop/Views/BlockHoldView.swift
@@ -10,23 +10,21 @@ import SwiftUI
 struct BlockHoldView: View {
         
     @State var blocks = [Block]()
-    @State var selectedBlockOffset = CGSize.zero
     
     var body: some View {
         HStack {
             ForEach(0..<blocks.count, id: \.self) { i in
-                var block = blocks[i]
+                let block = blocks[i]
                 block.image
                     .resizable()
                     .frame(width: 96, height: 96, alignment: .center)
-                    .offset(selectedBlockOffset)
+                    .offset(block.offset)
                     // Sets the offset to where the user drags the block
                     .gesture(
                         DragGesture()
                             .onChanged { gesture in
                                 block.offset = gesture.translation
-                                selectedBlockOffset = gesture.translation
-                                print(selectedBlockOffset)
+                                print(block.offset)
                             }
                         
                             .onEnded { _ in
@@ -41,7 +39,7 @@ struct BlockHoldView: View {
         .padding(20)
         .background(.green)
         .onAppear {
-            self.refillBlocks()
+            refillBlocks()
         }
     }
     
@@ -50,21 +48,26 @@ struct BlockHoldView: View {
     /// Refills the blocks to max, starting at the front of the block array
     func refillBlocks() {
         for _ in blocks.count..<maxBlocks {
-            let newBlock = Block(shape: [], image: Image("block-example"), offset: .zero)
+            @ObservedObject var newBlock = Block(shape: [], image: Image("block-example"))
             blocks.insert(newBlock, at: 0)
         }
     }
     
-    /// A struct representing each game piece used in the game
-    struct Block: Identifiable {
+    /// A class representing each game piece used in the game
+    class Block: ObservableObject {
         
         var id = UUID()
         /// 2D Array with 1 representing a block, 0 an empty tile
         var shape: [[Int]]
         /// The transparent image (png) that adheres to the shape of the block
-        var image: Image
+        @Published var image: Image
         /// The current x, y position on the screen relative to where the block starts
-        var offset: CGSize
+        @Published var offset = CGSize.zero
+        
+        init(shape: [[Int]], image: Image) {
+            self.shape = shape
+            self.image = image
+        }
         
     }
 }
