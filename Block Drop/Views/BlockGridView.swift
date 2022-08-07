750b3849006ffdd2f6dc3fdf8e9602794d8d879b
Traptricker <jackgeorgeelia@gmail.com>
2022-08-06 22:37:49 -0400

    Some grid setup

--- Block Drop/Views/BlockGridView.swift
+++ Block Drop/Views/BlockGridView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct BlockGridView: View {
+    
+    let gridWidth = 9
+    let gridHeight = 9
+    
+    private var grid: [[Int]]
+    
+    init() {
+        grid = [[Int]](repeating: [Int](repeating: 0, count: gridHeight), count: gridWidth)
+    }
+    
     var body: some View {
-        VStack {
-            Text("Game Grid Goes Here")
+        VStack(spacing: 0) {
+           drawGrid()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(10)
         .background(.blue)
     }
+    
+    @ViewBuilder func drawGrid() -> some View {
+        ForEach(grid, id: \.self) { row in
+            HStack(spacing: 0) {
+                ForEach(row, id: \.self) { column in
+                    Image("redsquare")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+                
+    }
+    
+    
 }
 
 struct BlockGridView_Previews: PreviewProvider {
