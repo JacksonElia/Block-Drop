c1b1db3b300c06527eb730820f7a4efae41d424d
Traptricker <jackgeorgeelia@gmail.com>
2022-08-06 23:57:43 -0400

    Some changes before I test something new

--- Block Drop/Views/BlockGridView.swift
+++ Block Drop/Views/BlockGridView.swift
@@ -12,34 +12,41 @@ struct BlockGridView: View {
     let gridWidth = 9
     let gridHeight = 9
     
-    private var grid: [[Int]]
+    @State var grid: [[Int]]
     
     init() {
+        // Creates the grid at the specified size
         grid = [[Int]](repeating: [Int](repeating: 0, count: gridHeight), count: gridWidth)
     }
     
     var body: some View {
         VStack(spacing: 0) {
-           drawGrid()
+            drawGrid()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(10)
         .background(.blue)
     }
     
+    /// Draws the grid each time it needs to be updated
     @ViewBuilder func drawGrid() -> some View {
-        ForEach(grid, id: \.self) { row in
+        ForEach(0..<grid.count, id: \.self) { row in
             HStack(spacing: 0) {
-                ForEach(row, id: \.self) { column in
-                    Image("redsquare")
-                        .resizable()
-                        .scaledToFit()
+                ForEach(0..<grid[row].count, id: \.self) { col in
+                    let cell = grid[row][col]
+                    if cell == 0 {
+                        Image("redsquare")
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Image("greensquare")
+                            .resizable()
+                            .scaledToFit()
+                    }
                 }
             }
         }
-                
     }
-    
     
 }
 
--- Block Drop/Views/BlockHoldView.swift
+++ Block Drop/Views/BlockHoldView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct BlockHoldView: View {
-    
-    @ObservedObject var blockContainer = BlockContainer()
+        
+    @State var blocks = [Block]()
+    @State var selectedBlockOffset = CGSize.zero
     
     var body: some View {
         HStack {
-            ForEach(0..<blockContainer.blocks.count, id: \.self) { i in
-                let block = blockContainer.blocks[i]
+            ForEach(0..<blocks.count, id: \.self) { i in
+                var block = blocks[i]
                 block.image
                     .resizable()
                     .frame(width: 96, height: 96, alignment: .center)
-                if i < blockContainer.blocks.count - 1 { // Spacers after everything but the last
+                    .offset(selectedBlockOffset)
+                    // Sets the offset to where the user drags the block
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                block.offset = gesture.translation
+                                selectedBlockOffset = gesture.translation
+                                print(selectedBlockOffset)
+                            }
+                        
+                            .onEnded { _ in
+                                // Do stuff for dropping the block
+                            }
+                    )
+                if i < blocks.count - 1 { // Spacers after everything but the last
                     Spacer()
                 }
             }
@@ -26,9 +41,31 @@ struct BlockHoldView: View {
         .padding(20)
         .background(.green)
         .onAppear {
-            blockContainer.refillBlocks()
-            print("refill")
+            self.refillBlocks()
         }
+    }
+    
+    let maxBlocks = 3
+    
+    /// Refills the blocks to max, starting at the front of the block array
+    func refillBlocks() {
+        for _ in blocks.count..<maxBlocks {
+            let newBlock = Block(shape: [], image: Image("block-example"), offset: .zero)
+            blocks.insert(newBlock, at: 0)
+        }
+    }
+    
+    /// A struct representing each game piece used in the game
+    struct Block: Identifiable {
+        
+        var id = UUID()
+        /// 2D Array with 1 representing a block, 0 an empty tile
+        var shape: [[Int]]
+        /// The transparent image (png) that adheres to the shape of the block
+        var image: Image
+        /// The current x, y position on the screen relative to where the block starts
+        var offset: CGSize
+        
     }
 }
 
