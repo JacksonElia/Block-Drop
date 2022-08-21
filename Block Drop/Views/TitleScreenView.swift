35dbf84a52807db76b553db27c693601852b6cb6
Traptricker <jackgeorgeelia@gmail.com>
2022-08-21 04:43:13 -0400

    Added button images

--- Block Drop/Views/TitleScreenView.swift
+++ Block Drop/Views/TitleScreenView.swift
@@ -19,66 +19,77 @@ struct TitleScreenView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding([.leading, .trailing], 10)
-                .padding([.bottom], -50)
+                .padding([.bottom], -30)
             Spacer()
-            Text("Play")
-                .onTapGesture {
-                    isOnTitleScreen = false
-                }
-            Spacer()
-            Text("Help")
-                .onTapGesture {
-                    helpShowing = true
-                }
-                .popover(isPresented: $helpShowing) {
-                    VStack {
-                        Text("Help")
-                        Spacer()
-                        VStack(alignment: .leading, spacing: 30) {
-                            Text("The goal of this game is to keep playing for as long as you can and to get as many points as possible.")
-                            Text("1. Place a block by dragging and dropping it onto the board.")
-                            Text("2. Keep an eye on the timer in the top left corner. If it hits 0, you lose. You can extend the time by placing a block.")
-                            Text("3. You get points and clear space when you fill a row, column, or subsection of the board up with blocks.")
-                        }
-                        .font(.custom("DINCondensed-Bold", size: 30))
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(30)
-                    .background(Color(0x393939))
+            VStack {
+                Image("start_up_button_\(getRandomButtonImageNumber()).play")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(10)
                     .onTapGesture {
-                        helpShowing = false
+                        isOnTitleScreen = false
                     }
-                }
-            Spacer()
-            Text("Credits")
-                .onTapGesture {
-                    creditsShowing = true
-                }
-                .popover(isPresented: $creditsShowing) {
-                    VStack {
-                        Text("Credits")
-                        Spacer()
-                        VStack(alignment: .leading, spacing: 30) {
-                            Text("This game is dedicated to our mother. I made it as a gift to her and so I could learn more about app development with SwiftUI.")
-                            Text("Programming done by me. Check out my [website](https://traptricker.github.io/)!")
-                                .onTapGesture {
-                                    UIApplication.shared.open(URL(string: "https://traptricker.github.io/")!, options: [:])
-
-                                }
-                            Text("Art done by my sibling.")
-                        }
-                        .font(.custom("DINCondensed-Bold", size: 30))
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(30)
-                    .background(Color(0x393939))
+                Spacer()
+                Image("start_up_button_\(getRandomButtonImageNumber()).help")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(10)
                     .onTapGesture {
-                        creditsShowing = false
+                        helpShowing = true
                     }
-                }
-            Spacer()
+                    .popover(isPresented: $helpShowing) {
+                        VStack {
+                            Text("Help")
+                            Spacer()
+                            VStack(alignment: .leading, spacing: 30) {
+                                Text("The goal of this game is to keep playing for as long as you can and to get as many points as possible.")
+                                Text("1. Place a block by dragging and dropping it onto the board.")
+                                Text("2. Keep an eye on the timer in the top left corner. If it hits 0, you lose. You can extend the time by placing a block.")
+                                Text("3. You get points and clear space when you fill a row, column, or subsection of the board up with blocks.")
+                            }
+                            .font(.custom("DINCondensed-Bold", size: 30))
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .padding(30)
+                        .background(Color(0x393939))
+                        .onTapGesture {
+                            helpShowing = false
+                        }
+                    }
+                Spacer()
+                Image("start_up_button_\(getRandomButtonImageNumber()).credits")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(10)
+                    .onTapGesture {
+                        creditsShowing = true
+                    }
+                    .popover(isPresented: $creditsShowing) {
+                        VStack {
+                            Text("Credits")
+                            Spacer()
+                            VStack(alignment: .leading, spacing: 30) {
+                                Text("This game is dedicated to our mother. I made it as a gift to her and so I could learn more about app development with SwiftUI.")
+                                Text("Programming done by me. Check out my [website](https://traptricker.github.io/)!")
+                                    .onTapGesture {
+                                        UIApplication.shared.open(URL(string: "https://traptricker.github.io/")!, options: [:])
+                                        
+                                    }
+                                Text("Art done by my sibling.")
+                            }
+                            .font(.custom("DINCondensed-Bold", size: 30))
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .padding(30)
+                        .background(Color(0x393939))
+                        .onTapGesture {
+                            creditsShowing = false
+                        }
+                    }
+                Spacer()
+            }
         }
         .font(.custom("DINCondensed-Bold", size: 60))
         .foregroundColor(.white)
@@ -86,6 +97,11 @@ struct TitleScreenView: View {
         .padding(20)
         .background(Color(0x393939))
     }
+    
+    func getRandomButtonImageNumber() -> Int {
+        return Int.random(in: 1...2)
+    }
+    
 }
 
 struct TitleScreenView_Previews: PreviewProvider {
