67e6e611d423789308d251c98ebb5b7c69e0f59f
Traptricker <jackgeorgeelia@gmail.com>
2022-08-21 04:17:12 -0400

    Added pop over windows

--- Block Drop/Views/TitleScreenView.swift
+++ Block Drop/Views/TitleScreenView.swift
@@ -22,30 +22,62 @@ struct TitleScreenView: View {
                 .padding([.bottom], -50)
             Spacer()
             Text("Play")
-                .foregroundColor(.white)
                 .onTapGesture {
                     isOnTitleScreen = false
                 }
             Spacer()
             Text("Help")
-                .foregroundColor(.white)
                 .onTapGesture {
                     helpShowing = true
                 }
                 .popover(isPresented: $helpShowing) {
-                    
+                    VStack {
+                        Text("Help")
+                        Spacer()
+                        VStack(alignment: .leading, spacing: 30) {
+                            Text("The goal of this game is to keep playing for as long as you can and to get as many points as possible.")
+                            Text("1. Place a block by dragging and dropping it onto the board.")
+                            Text("2. Keep an eye on the timer in the top left corner. If it hits 0, you lose. You can extend the time by placing a block.")
+                            Text("3. You get points and clear space when you fill a row, column, or subsection of the board up with blocks.")
+                        }
+                        .font(.custom("DINCondensed-Bold", size: 30))
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(30)
+                    .background(Color(0x393939))
+                    .onTapGesture {
+                        helpShowing = false
+                    }
                 }
             Spacer()
             Text("Credits")
-                .foregroundColor(.white).onTapGesture {
+                .onTapGesture {
                     creditsShowing = true
                 }
                 .popover(isPresented: $creditsShowing) {
-                    
+                    VStack {
+                        Text("Credits")
+                        Spacer()
+                        VStack(alignment: .leading, spacing: 30) {
+                            Text("This game is dedicated to our mother. I made it as a gift to her and so I could learn more about app development with SwiftUI.")
+                            Text("Programming done by me. Check out my [website](https://traptricker.github.io/)!")
+                            Text("Art done by my sibling.")
+                        }
+                        .font(.custom("DINCondensed-Bold", size: 30))
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(30)
+                    .background(Color(0x393939))
+                    .onTapGesture {
+                        creditsShowing = false
+                    }
                 }
             Spacer()
         }
         .font(.custom("DINCondensed-Bold", size: 60))
+        .foregroundColor(.white)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(20)
         .background(Color(0x393939))
